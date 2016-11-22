64befb31730cbd32bfe731fb17a7b83162d308e1
Sam Smith <sesmith177@gmail.com>
2016-11-22 10:39:04 -0500

    WIP; submodule already checked out
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- tasks/copy-cf-acceptance-tests/run.sh
+++ tasks/copy-cf-acceptance-tests/run.sh
@@ -6,8 +6,4 @@ set -o pipefail
 
 echo "Extracting cf-acceptance-tests from cf-release"
 
-pushd cf-release
-    git submodule update --init --recursive src/github.com/cloudfoundry/cf-acceptance-tests/
-popd
-
 rsync -a cf-release/src/github.com/cloudfoundry/cf-acceptance-tests/ cf-acceptance-tests/
