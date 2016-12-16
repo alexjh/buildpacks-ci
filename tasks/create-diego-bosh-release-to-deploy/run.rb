01f9bc5c746f5881327395ab811b13c4bae1da4e
Sam Smith <sesmith177@gmail.com>
2016-12-16 13:45:50 -0500

    Adapt diego-release to always return true for container check as well
    Signed-off-by: James Wen <jrw2175@columbia.edu>

---
+++ tasks/create-diego-bosh-release-to-deploy/run.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'fileutils'
+require 'yaml'
+
+buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+require "#{buildpacks_ci_dir}/lib/cf-release-common"
+
+replacement_function = <<-FUNCTION
+running_in_container() {
+  true
+}
+FUNCTION
+
+Dir.chdir 'diego-release' do
+  files_to_edit = Dir["**/*.sh"].select do |file|
+    File.read(file).include? 'running_in_container()'
+  end
+
+  files_to_edit.each do |file|
+    contents=File.read(file)
+
+    contents.gsub!(/running_in_container\(\)\s+{.*}/m, replacement_function )
+    File.write(file, contents)
+    puts "Replaced running_in_container() in #{file}"
+  end
+
+  system(%(bundle && bosh --parallel 10 sync blobs && bosh create release --force --with-tarball --name diego)) || raise('cannot create diego-release')
+end
+
+system('rsync -a diego-release/ diego-release-artifacts') || raise('cannot rsync directories')
