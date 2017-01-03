1521c12cb995b559a3057e5acda6bdc1886bb30b
Anna Thornton <athornton@pivotal.io>
2017-01-03 14:24:09 -0500

    WIP; use BuildpackCVEHistory instead of CVEHistory in stackscvenotifier

--- tasks/check-for-new-rootfs-cves/run.rb
+++ tasks/check-for-new-rootfs-cves/run.rb
@@ -6,15 +6,14 @@ buildpacks_ci_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..
 require "#{buildpacks_ci_dir}/lib/buildpack-cve-tags"
 require "#{buildpacks_ci_dir}/lib/buildpack-cve-notifier"
 require "#{buildpacks_ci_dir}/lib/stack-cve-notifier"
-require "#{buildpacks_ci_dir}/lib/cve-history"
 require "#{buildpacks_ci_dir}/lib/notifiers/system-cve-tracker-notifier"
-require "#{buildpacks_ci_dir}/lib/notifiers/system-cve-slack-notifier"
-require "#{buildpacks_ci_dir}/lib/notifiers/system-cve-email-preparer-and-github-issue-notifier"
+require "#{buildpacks_ci_dir}/lib/notifiers/buildpack-system-cve-slack-notifier"
+require "#{buildpacks_ci_dir}/lib/notifiers/buildpack-system-cve-email-preparer-and-github-issue-notifier"
 
 stacks_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'stacks'))
 
 if ENV['STACK'] == 'stacks'
-  notifiers = [SystemCVEEmailPreparerAndGithubIssueNotifier, SystemCVETrackerNotifier, SystemCVESlackNotifier]
+  notifiers = [BuildpackSystemCVEEmailPreparerAndGithubIssueNotifier, SystemCVETrackerNotifier, BuildpackSystemCVESlackNotifier]
   cves_dir = File.expand_path(File.join(buildpacks_ci_dir, '..', 'output-new-cves', 'new-cve-notifications'))
 elsif ENV['STACK'] == 'stacks-nc'
   notifiers = []
@@ -23,6 +22,4 @@ else
   raise "Unsupported stack: #{ENV['STACK']}"
 end
 
-cve_history = CVEHistory.new(cves_dir)
-
-StackCVENotifier.new(cve_history, cves_dir, stacks_dir).run!('Ubuntu 14.04', 'ubuntu14.04', notifiers)
+StackCVENotifier.new(cves_dir, stacks_dir).run!('Ubuntu 14.04', 'ubuntu14.04', notifiers)
