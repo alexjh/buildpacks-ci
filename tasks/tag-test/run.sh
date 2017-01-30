2b42cba4f86738fbf89f1545c50070a310d0ac81
Sam Smith <sesmith177@gmail.com>
2017-01-30 10:01:31 -0500

    Copy to output directory
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- tasks/tag-test/run.sh
+++ tasks/tag-test/run.sh
@@ -4,8 +4,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-cd buildpack
-echo "test-tag" >> "test.txt"
-git add test.txt
-git commit -m "Adding a test tag file"
-git tag "vtest-tag"
+pushd buildpack
+	echo "test-tag" >> "test.txt"
+	git add test.txt
+	git commit -m "Adding a test tag file"
+	git tag "vtest-tag"
+popd
+
+rsync -a buildpack/ buildpack-output
