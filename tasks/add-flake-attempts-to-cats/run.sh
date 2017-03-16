8464765f8b28ee14c4924647d5d8471f520613a3
Anna Thornton <athornton@pivotal.io>
2017-03-16 16:04:18 -0400

    wip
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- tasks/add-flake-attempts-to-cats/run.sh
+++ tasks/add-flake-attempts-to-cats/run.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-rsync -vaz cats-concourse-task-inp cats-concourse-task
+rsync -vaz cats-concourse-task-inp/ cats-concourse-task/
 
 sed -i s/keepGoing/-flakeAttempts=3/ cats-concourse-task/task
