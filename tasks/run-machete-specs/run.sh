020517d0c4a16baaa244d8bfe3eb4ecf0e92c466
Dave Goddard <dave@goddard.id.au>
2017-03-16 10:02:41 -0400

    wip

--- tasks/run-machete-specs/run.sh
+++ tasks/run-machete-specs/run.sh
@@ -8,6 +8,8 @@ pushd buildpacks-ci
   ./scripts/start-docker
 popd
 
+./cf-space/login
+
 pushd machete
   if [ ! -z "$RUBYGEM_MIRROR" ]; then
     bundle config mirror.https://rubygems.org "${RUBYGEM_MIRROR}"
